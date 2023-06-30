9e7cc2b0402634fe45a0832a25a2b0de7939aa3a
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2023-06-30 15:35:08 +0300

    sync

--- start.py
+++ start.py
@@ -3,8 +3,12 @@ from card import Card
 from hand import Hand
 from game import Game
 
+#function to start game
 def start():
+    #create Game class instance
     g = Game()
+    #call play method on game class instance
     g.play()
 
+#run start function to start game
 start()
